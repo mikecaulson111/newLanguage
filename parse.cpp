9e03384099de516b15c9d4f4e6da2dc11ca518dd
Mikecaulson <mikecaulson111@gmail.com>
2024-11-04 11:21:10 -0700

    testing commit

--- parse.cpp
+++ parse.cpp
@@ -11,6 +11,9 @@ using namespace std;
 int main() {
     
     vector<Token> tokens;
+    tokens.push_back({NUMBER, "1"});
+    tokens.push_back({OPERATOR, "+"});
+    tokens.push_back({NUMBER, "17"});
 
 
     return 0;
